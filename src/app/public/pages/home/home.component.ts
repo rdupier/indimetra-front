524278092f4f71e0320bc8e5fdba96419ec1aa2e
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-04-14 00:44:04 +0200

    fix(navbar/home): ajusto detalles de los componentes home y navbar

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CortometrajeService } from '../../../auth/services/cortometraje.service';
+import { Cortometraje } from '../../../auth/interfaces/cortometraje.interface';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html'
+  standalone: true,
+  templateUrl: './home.component.html',
+  imports: [RouterModule],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  cortometrajes: Cortometraje[] = [];
 
+  constructor(private cortometrajeService: CortometrajeService) {}
+
+  ngOnInit(): void {
+    this.cortometrajeService.getAllCortometrajes().subscribe({
+      next: (data) => (this.cortometrajes = data),
+      error: (err) => console.error('Error al cargar cortometrajes:', err),
+    });
+  }
 }
