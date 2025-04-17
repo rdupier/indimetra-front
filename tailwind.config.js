7eb508c4211d0b9760aacbcc418ddae6b40f7cfc
Patricia <pgutierrezrivas@gmx.es>
2025-04-17 14:51:32 +0200

    Add styles to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,55 @@ module.exports = {
       fontFamily: {
         sans: ['Manrope', 'sans-serif'],
       },
+      fontSize: {
+        base: ['1rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.002rem',
+          fontWeight: '500',
+        }],
+        bodyS: ['0.875rem', {
+          lineHeight: '1.25rem',
+          letterSpacing: '0.00175rem',
+          fontWeight: '500',
+        }],
+        bodyXS: ['0.75rem', {
+          lineHeight: '1rem',
+          letterSpacing: '0.00375rem',
+          fontWeight: '500',
+        }],
+        actionBase: ['1rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.002rem',
+          fontWeight: '600',
+        }],
+        actionS: ['0.875rem', {
+          lineHeight: '1.25rem',
+          letterSpacing: '0.00175rem',
+          fontWeight: '600',
+        }],
+        actionXS: ['0.75rem', {
+          lineHeight: '1rem',
+          letterSpacing: '0.00375rem',
+          fontWeight: '600',
+        }],
+        h2: ['1.75rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '600',
+          fontFamily: 'Manrope',
+        }],
+        h3: ['1.625rem', {
+          lineHeight: '3.0625rem',
+          fontWeight: '600',
+        }],
+        h4: ['1.375rem', {
+          lineHeight: '2.5625rem',
+          fontWeight: '600',
+        }],
+        h5: ['1.25rem', {
+          lineHeight: '2.125rem',
+          fontWeight: '600',
+        }],
+      },
     },
   },
   plugins: [require("daisyui")],
