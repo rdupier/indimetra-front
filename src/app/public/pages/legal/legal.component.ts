e2f02fec3d664ee986a2b30c49d087ca220b74d6
Patricia <pgutierrezrivas@gmx.es>
2025-04-18 11:25:19 +0200

    Create contact page

--- src/app/public/pages/legal/legal.component.ts
+++ src/app/public/pages/legal/legal.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-legal',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './legal.component.html',
   styleUrl: './legal.component.css'
 })
