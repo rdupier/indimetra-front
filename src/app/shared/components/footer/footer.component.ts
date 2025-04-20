ed637cacbb0c8fd0a51184c931d53abe7ae1a264
Ruth Dupiereux <ruthdupiereux@gmail.com>
2025-04-20 17:03:13 +0200

    feat(footer): show/hide footer links based on user role

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  private authService = inject(AuthService);
+  isAdmin = this.authService.isAdmin;
 }
